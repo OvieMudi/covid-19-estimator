f44fe92f3e9559d51c97daeaf49ab8e7fd8eb97d
OvieMudi <oviisaacs@gmail.com>
2020-04-10 09:49:12 +0100

    complete challenge 1
    - write function for calculating the estimate & high impact
    - handle normalization of period in months, weeks, and days
    
    [Delivers 1#issue-597777240 ]

--- src/estimator.js
+++ src/estimator.js
@@ -1,6 +1,61 @@
-const covid19ImpactEstimator = (data) => data;
 
-console.log('Going live in 5, 4, 3...');
+const input = {
+  region: {
+    name: 'Africa',
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+  },
+  periodType: 'days',
+  timeToElapse: 38,
+  reportedCases: 2747,
+  population: 66622705,
+  totalHospitalBeds: 1380614
+};
+
+const normalizeTimeToElapse = (period, periodType) => {
+  let normalizedTime = 1;
+
+  if (['day', 'days'].includes(periodType)) {
+    normalizedTime = period;
+  } else if (['week', 'weeks'].includes(periodType)) {
+    normalizedTime = period * 7;
+  } else if (['month', 'months'].includes(periodType)) {
+    normalizedTime = period * 30;
+  }
+
+  return normalizedTime;
+};
+
+const estimateInfections = (reportedCases, periodType, timeToElapse) => (isSevereImpact) => {
+  const currentlyInfected = isSevereImpact ? reportedCases * 50 : reportedCases * 10;
+  return {
+    currentlyInfected,
+    infectionsByRequestedTime: currentlyInfected * (
+      2 ** Math.floor(normalizeTimeToElapse(timeToElapse, periodType) / 3))
+  };
+};
+
+
+const covid19ImpactEstimator = (data) => {
+  const { reportedCases, periodType, timeToElapse } = data;
+
+  console.log('covid19ImpactEstimator -> data', data);
+
+  const estimatedInfections = estimateInfections(reportedCases, periodType, timeToElapse);
+
+  return {
+    data,
+    estimate: {
+      currentlyInfected: estimatedInfections(false).currentlyInfected,
+      infectionsByRequestedTime: estimatedInfections(false).infectionsByRequestedTime
+    },
+    severeImpact: {
+      currentlyInfected: estimatedInfections(true).currentlyInfected,
+      infectionsByRequestedTime: estimatedInfections(true).infectionsByRequestedTime
+    }
+  };
+};
 
 
 export default covid19ImpactEstimator;
