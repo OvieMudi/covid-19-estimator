9277a84778d3cab784c34d36ca6b9253f1f2983c
OvieMudi <oviisaacs@gmail.com>
2020-04-14 16:26:49 +0100

    update response format

--- src/server/app.js
+++ src/server/app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 import appRoot from 'app-root-path';
+import cors from 'cors';
 import onCovidRouter from './routes';
 
 
@@ -11,12 +12,12 @@ dotenv.config();
 const app = express();
 
 const accessLogStream = fs.createWriteStream(`${appRoot}/src/log/request.txt`, { flags: 'a' });
-const format = '{"method":":method", "url":":url", "status"::status, "responseTime":":response-time"}';
+const format = ':method\t\t:url\t\t:status\t\t0:response-time[0]ms';
 
 app.use(morgan(format, { stream: accessLogStream }));
 app.use(express.json());
+app.use(cors());
 app.use('/api/v1', onCovidRouter);
-app.disable('x-powered-by');
 
 
 const PORT = process.env.PORT || 3100;
--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -1,21 +1,16 @@
-
-import { readFileSync } from 'fs';
 import xml2js from 'xml2js';
 import appRoot from 'app-root-path';
 import estimator from '../../estimator';
 
-const toXML = new xml2js.Builder();
+const toXML = new xml2js.Builder({ headless: true });
 
 
 const estimatorJsonController = (req, res) => {
-  console.log('estimatorJsonController -> req.body', req.body);
-
   const estimate = estimator(req.body);
   res.status(200).json(estimate);
 };
 
 const estimatorXMLController = (req, res) => {
-  console.log('estimatorXMLController -> req.body', req.body);
   const estimate = estimator(req.body);
 
   const XMLString = toXML.buildObject(estimate);
@@ -27,21 +22,8 @@ const estimatorXMLController = (req, res) => {
 
 
 const logController = (req, res) => {
-  console.log('logController -> req.body', req.body);
-  let parsedLog = '';
-
-  const logs = readFileSync(`${appRoot}/src/log/request.txt`, 'utf8').split('\n');
-
-  let logLine = {};
-
-  logs.forEach((line) => {
-    if (line.includes('method')) {
-      logLine = JSON.parse(line);
-      parsedLog += `${logLine.method} ${logLine.url} ${logLine.status} ${Math.ceil(Number(logLine.responseTime))}ms\n`;
-    }
-  });
-
-  return res.send(parsedLog);
+  res.setHeader('Content-Type', 'text/plain');
+  return res.sendFile(`${appRoot}/src/log/request.txt`);
 };
 
 export { estimatorJsonController, estimatorXMLController, logController };
